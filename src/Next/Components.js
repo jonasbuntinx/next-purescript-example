c75bcd560cca4a52c7be2c655aca6d4a4e4c9004
Jonas Buntinx <buntinxjonas@gmail.com>
2019-03-01 18:56:07 +0900

    Removed commented out code

--- src/Next/Components.js
+++ src/Next/Components.js
@@ -5,7 +5,6 @@ var Link = require('next/link').default;
 
 exports.link = function() {
   return function(props) {
-    //props.children = React.createElement.apply(null, [React.Fragment, {}].concat(props.children));
     return React.createElement(Link, props);
   }
 }
