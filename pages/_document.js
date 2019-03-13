180fe256f390b6bdf3233768ba3b6c26331f3adb
Jonas Buntinx <buntinxjonas@gmail.com>
2019-03-13 18:06:49 +0900

    Made _document export anonymous

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document from 'next/document';
 import { render } from '../output/Custom.Document/index.js';
 
-export default class MyDocument extends Document {
+export default class extends Document {
   render = render;
 }
