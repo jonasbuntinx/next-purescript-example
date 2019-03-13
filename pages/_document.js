f6744aa0ff6e5e01bc9a33a37fbf5b93410ba285
Jonas Buntinx <buntinxjonas@gmail.com>
2019-03-13 18:02:24 +0900

    Using ES6 classes in _document instead of ES5

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,6 @@
 import Document from 'next/document';
 import { render } from '../output/Custom.Document/index.js';
 
-function MyDocument() {
-  return Document.apply(this, arguments) || this;
-};
-
-MyDocument.prototype = Object.create(Document.prototype);
-MyDocument.prototype.constructor = MyDocument;
-MyDocument.__proto__ = Document;
-MyDocument.prototype.render = render;
-
-export default MyDocument;
+export default class MyDocument extends Document {
+  render = render;
+}
